b780e86734b9a0e8daf1e165d36ce7f576b84170
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-05-15 16:47:49 +0900

    メモ一覧のレンダリング

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
             ->orderBy('updated_at', 'DESC')
             ->get();
         // dd($memos);
-        return view('create');
+        return view('create', compact('memos'));
     }
 
     public function store(Request $request)
