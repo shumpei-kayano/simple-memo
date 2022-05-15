8c07865b6226e06ab1ed0705ab9bc554160d3382
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-05-15 15:29:29 +0900

    メモ作成機能の開発（View側）

---
+++ resources/views/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        新規メモ作成
+    </div>
+    <form class="card-body" action="/sotre" method="POST">
+        @csrf
+        <div class="mb-3">
+            <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力"></textarea>
+          </div>
+          <button type="button" class="btn btn-primary">保存</button>
+    </form>
+</div>
+@endsection
